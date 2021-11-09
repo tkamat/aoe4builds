2b64b7ba763203f5a6406605c88eb3bece976674
Tushaar Kamat <platypusdiamond@gmail.com>
2021-11-08 22:51:40 -0500

    Add civ flags to homepage

--- src/App.js
+++ src/App.js
@@ -1,55 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import abbasid from './images/abbasid.png';
+import chinese from './images/chinese.png';
+import delhi from './images/delhi.png';
+import england from './images/england.png';
+import french from './images/french.svg';
+import hre from './images/hre.png';
+import mongols from './images/mongols.png';
+import rus from './images/rus.png';
 import './App.css';
+import { Card, CardGroup, Col, Row } from 'react-bootstrap';
+import Button from '@restart/ui/esm/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <CardGroup>
+              <Card>
+                <Card.Img variant="top" src={abbasid} />
+                <Card.Body>
+                  <Card.Title>England</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={chinese} />
+                <Card.Body>
+                  <Card.Title>Chinese</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={delhi} />
+                <Card.Body>
+                  <Card.Title>Delhi</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={england} />
+                <Card.Body>
+                  <Card.Title>England</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={french} />
+                <Card.Body>
+                  <Card.Title>French</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={hre} />
+                <Card.Body>
+                  <Card.Title>Holy Roman Empire</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={mongols} />
+                <Card.Body>
+                  <Card.Title>Mongols</Card.Title>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Img variant="top" src={rus} />
+                <Card.Body>
+                  <Card.Title>Rus</Card.Title>
+                </Card.Body>
+              </Card>
+              </CardGroup>
       </header>
     </div>
   );
